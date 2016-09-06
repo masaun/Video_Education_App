180edb17e7de58c64274833ab299fca4bc9c7c4c
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-09-06 12:31:10 +0900

    Authenticate with Google account

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,33 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
 
 
   validates :name, presence: true, length: {maximum: 25}
+
+  
+
+  def self.find_for_google_oauth2(acccess_token, signed_in_resourse=nil)
+    data = acccess_token.info
+    user = User.where(:provider => acccess_token.provider, :uid => acccess_token.uid).first
+
+    if user
+      return user
+    else
+      registered_user = User.where(:email => acccess_token.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(
+          name: data["name"],
+          provider: acccess_token.provider,
+          email: data["email"],
+          uid: acccess_token.uid,
+          password: Devise.friendly_token[0,20]
+        )
+      end
+    end
+  end
 end
