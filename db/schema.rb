7bd3e27113d089f6d7ff6e0703ec17180cd49fb5
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-09-07 13:35:43 +0900

    Models for Project & Task by association and strong_perameter and validates and so on

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907032411) do
+ActiveRecord::Schema.define(version: 20160907040749) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -46,6 +46,27 @@ ActiveRecord::Schema.define(version: 20160907032411) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "projects", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "content",    limit: 65535
+    t.integer  "price",      limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "note",       limit: 65535
+    t.string   "video",      limit: 255
+    t.boolean  "header",                   default: false, null: false
+    t.string   "tag",        limit: 255
+    t.integer  "project_id", limit: 4
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+  end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -68,4 +89,5 @@ ActiveRecord::Schema.define(version: 20160907032411) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "tasks", "projects"
 end
