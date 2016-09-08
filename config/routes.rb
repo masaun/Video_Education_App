e56c12e0c130a1f5ec7bf5f86021249ae78756af
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-09-08 12:30:12 +0900

    Controllers, Views for Project & Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'task/show'
+
+  get 'project/index'
+
+  get 'project/show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
@@ -6,7 +12,12 @@ Rails.application.routes.draw do
 
   get 'pages/about'
 
-  root 'pages#about'
+  root 'project#index'
+
+  resources :project do
+    resources :task, only: [:show]
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
