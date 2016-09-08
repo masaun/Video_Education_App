fa0332337315eccd7f25edd23f06004f35070562
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-09-08 22:35:04 +0900

    Drag and Drop in Dashboard by Gem active_admin-sortable_tree'

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'redcarpet', '~> 3.2', '>= 3.2.3'
 gem 'coderay', '~> 1.1'
 
 gem 'friendly_id', '~> 5.1'
+
+gem 'active_admin-sortable_tree', '~> 0.2.1'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -9,4 +9,8 @@ class Project < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "680x300>", thumb: "175x75>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def shortname
+    name.length > 25? name[0..25] + "..." : name
+  end
 end
